3492f9d1913ca28d17323f034703438c54c9c6b9
janvirighthere <janvier.bur@gmail.com>
2021-02-24 00:36:38 -0500

    solved triangle exercise

--- intro/src/Triangle.java
+++ intro/src/Triangle.java
@@ -1,2 +1,11 @@
 public class Triangle {
+    public static void main(String[] args) {
+        int height = 5;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
