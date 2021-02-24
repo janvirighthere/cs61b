26dec766e872935858afc8af8e1e4ace640ebaa8
janvirighthere <janvier.bur@gmail.com>
2021-02-24 00:22:35 -0500

    While loops

--- intro/src/WhileLoops.java
+++ intro/src/WhileLoops.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class WhileLoops {
+public class WhileLoops {
+    public static void main(String[] args) {
+        int bottles = 90;
+        int counter = 0;
+
+        while (bottles > 0) {
+            System.out.println(bottles + " bottles of beer on the wall!");
+            System.out.println("Counter is " + counter);
+            bottles -= 1;
+            counter += 1;
+        }
+
+        int bottles2 = 5;
+        counter = 9;
+        while (bottles2 > 0) {
+            bottles = -312;
+            System.out.println(bottles + " bottles of beer on the wall!");
+            System.out.println("Counter is " + counter);
+            counter += 1;
+
+        }
+
+    }
 }
